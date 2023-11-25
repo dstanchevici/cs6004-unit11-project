e6a10673d24cafa0253a48e0236fac785a8eb0b5
dstanchevici <distanchev@gmail.com>
2023-11-25 10:48:41 -0500

    finished manager's decision form in html and js

--- manageapplication.js
+++ manageapplication.js
@@ -1,9 +1,9 @@
 // Servlet's URL
 const url = "http://localhost:40104/manageServlet";
 
-/*let applicantInfoFromServer = {
-    uid:33,
-    firstName:"Roman",
+let applicantInfoFromServer = {
+    uid:null,
+    firstName:null,
     lastName:null,
     age:null,
     email:null,
@@ -15,7 +15,13 @@ const url = "http://localhost:40104/manageServlet";
     locationAssignment:null,
     jobAssignment:null,
     reviewDate:null
-}*/
+}
+
+// Get all vacancies with a method that has already been created in ManageServlet,
+// but use only vacanciesFromServer[i].location in this page.
+// A bit inefficient, but there is less code b/c I don't need to create a customized method
+// (such as getLocations()) in ManageServlet.
+let vacanciesFromServer = []
 
 
 const app = angular.module("myApp", ['ngMaterial', 'ngMessages']);
@@ -28,10 +34,18 @@ app.config(function($mdThemingProvider) {
 });
 
 app.controller("myController", function($scope) {
+    $scope.applicantInfo = {};
     getAndDisplayApplicantInfo($scope);
+
+    $scope.locations = []; // Array of objects (location, index)
+    getAndDisplayLocations($scope);
+
     $scope.statusIndex = -1;
-    $scope.submitDecision = function (uid) {
-        console.log ("Submit statusIndex " + $scope.statusIndex + " for " + uid);
+    $scope.locationIndex = -1;
+    $scope.jobIndex = -1;
+    $scope.deleteApplication = false;
+    $scope.submitDecision = function () {
+        printInfo($scope);
     };
 });
 
@@ -54,10 +68,48 @@ function getAndDisplayApplicantInfo($scope) {
     req.onload = () => {
         console.log ("req.response: " + req.response);
         const dataAsJsonObj = JSON.parse(req.response);
-        let applicantInfoFromServer = dataAsJsonObj;
-        console.log ("in req.onload applicantInfoFromServer: " + applicantInfoFromServer.firstName);
+        applicantInfoFromServer = dataAsJsonObj;
         $scope.applicantInfo = applicantInfoFromServer;
     }
 
 }
+
+function getAndDisplayLocations($scope) {
+    let dataToServer = {};
+    dataToServer.servletAction = "getVacancies";
+
+    console.log ("dataToServer.servletAction: " + dataToServer.servletAction);
+    console.log ("To be sent to server: json=" + JSON.stringify(dataToServer));
+
+    let req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.open("POST", url);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify(dataToServer));
+    console.log ("Sent to server: json=" + JSON.stringify(dataToServer));
+
+    req.onload = () => {
+        console.log ("req.response: " + req.response);
+        const dataAsJsonObj = JSON.parse(req.response);
+        vacanciesFromServer = dataAsJsonObj;
+        //$scope.locations = []; // Array of objects (location, index)
+        for (let i=0; i<vacanciesFromServer.length; i++) {
+            let locationObject = {location:vacanciesFromServer[i].location, index:i+1};
+            $scope.locations.push(locationObject);
+        }
+    }
+
+}
+
+function printInfo ($scope) {
+    console.log ("In printInfo(): applicantInfoFromServer " + applicantInfoFromServer.firstName);
+    console.log ("In printInfo(): $scope.applicantInfo.status " + $scope.applicantInfo.status);
+    console.log("UID " + $scope.applicantInfo.uid + " has statusIndex " + $scope.statusIndex);
+    console.log("UID " + $scope.applicantInfo.uid + " has locationIndex " + $scope.locationIndex);
+    console.log("UID " + $scope.applicantInfo.uid + " has jobIndex " + $scope.jobIndex);
+    console.log("UID " + $scope.applicantInfo.uid + " applic. marked for deletion " + $scope.deleteApplication);
+
+}
+
 
