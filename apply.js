e82a35ccff39b8fe822fe571fd9423327f551277
dstanchevici <distanchev@gmail.com>
2023-11-26 14:54:01 -0500

    apply form: added  values to be checked and sent to server

--- apply.js
+++ apply.js
@@ -11,4 +11,56 @@ app.config(function($mdThemingProvider) {
 
 app.controller("myController", function($scope) {
     $scope.uid = sessionStorage.getItem("uid");
+    $scope.firstName = null;
+    $scope.lastName = null;
+    $scope.email = null;
+    $scope.age = null;
+    $scope.locationPreference = null;
+    $scope.jobPreferenceIndex = null;
+    $scope.skatingSkill = null;
+
+    $scope.locations = []; // Array of locations as strings
+    getLocationsFromServer($scope);
+
+    $scope.submit = function() {
+        checkData ($scope);
+    };
 });
+
+function getLocationsFromServer($scope) {
+    let dataToServer = {};
+    dataToServer.servletAction = "getVacancies";
+
+    console.log ("dataToServer.servletAction: " + dataToServer.servletAction);
+    console.log ("To be sent to server: json=" + JSON.stringify(dataToServer));
+
+    let req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.open("POST", url);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send(JSON.stringify(dataToServer));
+    console.log ("Sent to server: json=" + JSON.stringify(dataToServer));
+
+    req.onload = () => {
+        //console.log ("req.response: " + req.response);
+        const dataAsJsonObj = JSON.parse(req.response);
+        const vacanciesFromServer = dataAsJsonObj;
+        //$scope.locations = []; // Array of objects (location, deskvacancies, icevacancies, index)
+        for (let i=0; i<vacanciesFromServer.length; i++) {
+            $scope.locations.push(vacanciesFromServer[i].location);
+        }
+    }
+
+}
+
+function checkData($scope) {
+    console.log ("Input:");
+    console.log($scope.firstName);
+    console.log($scope.lastName);
+    console.log($scope.email);
+    console.log($scope.age);
+    console.log($scope.locationPreference);
+    console.log($scope.jobPreferenceIndex);
+    console.log($scope.skatingSkill);
+}
